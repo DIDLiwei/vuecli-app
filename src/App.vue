2089994136da1d0d39174e80280e97836d3ade4e
DIDLiwei <623790416@qq.com>
2019-08-30 17:39:52 +0800

    vuecli-app

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+// 展示模板
+<template>
+  <div id="app">
+    <h1>{{title}}</h1>
+    <p>{{greeting()}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      title: "this is app.vue"
+    };
+  },
+  methods: {
+    greeting() {
+      return "Hello Vue.js";
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ vuecli.md
@@ -0,0 +1,7 @@
+## vue创建项目流程
+##1.通过cd切换到你的项目存放的路径 例如vue-vuex-app
+##2.vue create 项目名称
+##3.cd项目路径下
+##4.通过vscode打开项目
+##5.ctrl + ~ 打开terminal
+##6.npm run serve 运行项目
